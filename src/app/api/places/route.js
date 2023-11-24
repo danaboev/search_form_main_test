ea435449c26f8f7cdcca3fccd735204ca201c6a5
danaboev <danaboev.jf@gmail.com>
2023-11-24 20:17:20 +0500

    first

---
+++ src/app/api/places/route.js
@@ -0,0 +1,10 @@
+// import { BaseNextResponse } from "next/dist/server/base-http";
+
+// async function fetchPlaces(){
+//     const response = await fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-34.397,150.644&radius=500&type=restaurant&keyword=food&key=<KEY>');
+//     const data = await response.json();
+//     return data.results;
+
+// }
+
+ ///data request GET
